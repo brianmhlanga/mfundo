8f0963364cb349bb6131c6edba63fb9b6d857e1b
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-01-10 16:56:52 +0200

    changes

---
+++ components/landing/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="relative p-8 overflow-hidden bg-no-repeat bg-cover" style="background-image: url(&quot;https://blocks.primevue.org/images/blocks/hero/hero-3.jpg&quot;);">
+        <div class="text-center my-6 relative">
+            <div class="text-6xl font-bold mb-1 text-white">Ace Your Exams with</div>
+            <div class="text-6xl text-blue-400 font-bold mb-4">Mfundo!</div>
+            <p class="mt-0 mb-3 line-height-3 text-center mx-auto text-blue-100" style="max-width: 500px;">Independent Exam Creation, Answer Selection, Customizable Time Limits.</p>
+            <div class="flex align-items-center justify-content-center flex-wrap">
+            <button class="p-button p-component p-3 mt-3" type="button" aria-label="Notify Me" data-pc-name="button" data-pc-section="root" data-pd-ripple="true" style="border-radius: 40px;">
+                <!---->
+                <span class="p-button-label" data-pc-section="label">Explore Exams</span>
+                <!---->
+                <span role="presentation" aria-hidden="true" data-p-ink="true" data-p-ink-active="false" class="p-ink" data-pc-name="ripple" data-pc-section="root"></span>
+            </button>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/landing/third.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
+    <div class="font-bold text-900 text-3xl mb-3 text-center">Key Features to Boost Your Revision</div>
+    <div class="flex flex-wrap">
+        <div class="w-full lg:w-6 xl:w-3 p-5">
+        <img src="https://blocks.primevue.org/images/blocks/feature/feature-illustration-1.svg" alt="Image" class="w-full">
+        <div class="mt-3 mb-2 font-medium text-900 text-xl">Independent Exam Creation</div>
+        <span class="text-700 line-height-3">Create custom exams tailored to your study needs. Handpick questions from our extensive catalog or add your own.</span>
+        <a tabindex="0" class="text-blue-500 font-medium flex align-items-center mt-2">
+            <span>Explore</span>
+            <i class="pi pi-arrow-right ml-3"></i>
+        </a>
+        </div>
+        <div class="w-full lg:w-6 xl:w-3 p-5">
+        <img src="https://blocks.primevue.org/images/blocks/feature/feature-illustration-2.svg" alt="Image" class="w-full">
+        <div class="mt-3 mb-2 font-medium text-900 text-xl">Answer Selection</div>
+        <span class="text-700 line-height-3">Choose and predetermine your answers. Practice strategic answering and reinforce your knowledge.</span>
+        <a tabindex="0" class="text-blue-500 font-medium flex align-items-center mt-2">
+            <span>Explore</span>
+            <i class="pi pi-arrow-right ml-3"></i>
+        </a>
+        </div>
+        <div class="w-full lg:w-6 xl:w-3 p-5">
+        <img src="https://blocks.primevue.org/images/blocks/feature/feature-illustration-3.svg" alt="Image" class="w-full">
+        <div class="mt-3 mb-2 font-medium text-900 text-xl">Customizable Time Limits</div>
+        <span class="text-700 line-height-3">Set time limits for each exam session. Mimic real exam conditions and improve time management skills.</span>
+        <a tabindex="0" class="text-blue-500 font-medium flex align-items-center mt-2">
+            <span>Explore</span>
+            <i class="pi pi-arrow-right ml-3"></i>
+        </a>
+        </div>
+        <div class="w-full lg:w-6 xl:w-3 p-5">
+        <img src="https://blocks.primevue.org//images/blocks/feature/feature-illustration-4.svg" alt="Image" class="w-full">
+        <div class="mt-3 mb-2 font-medium text-900 text-xl">Comprehensive Exam Catalog</div>
+        <span class="text-700 line-height-3">Explore a vast catalog of exams spanning various subjects and difficulty levels. Find the perfect practice material</span>
+        <a tabindex="0" class="text-blue-500 font-medium flex align-items-center mt-2">
+            <span>Explore</span>
+            <i class="pi pi-arrow-right ml-3"></i>
+        </a>
+        </div>
+<div class="surface-section px-4 py-8 md:px-6 lg:px-8">
+  <div class="text-center text-900 text-5xl font-bold mb-5">Featured Exams</div>
+  <div class="grid grid-nogutter">
+    <div class="col-12 lg:col-3 p-3">
+      <div class="p-3 custom-shadow-2 border-round surface-card h-full">
+        <div class="flex justify-content-between text-500">
+          <span>Article</span>
+          <span class="inline-flex">
+            <i class="pi pi-clock mr-2"></i>
+            <span>12 min</span>
+          </span>
+        </div>
+        <p class="line-height-3 mt-3 mb-7 text-900 font-medium text-xl">It is time to step up and protect our world. Little actions will lead to huge differences.</p>
+        <div>
+          <span class="bg-blue-100 text-blue-600 font-medium py-1 px-3 mr-2" style="border-radius: 30px;">Environment</span>
+          <span class="bg-teal-100 text-teal-600 font-medium py-1 px-3" style="border-radius: 30px;">Climate</span>
+        </div>
+        <img src="https://blocks.primevue.org/images/blocks/blog/blog-6.png" alt="Image" class="block w-full mt-5">
+      </div>
+    </div>
+    <div class="col-12 lg:col-3 p-3">
+      <div class="p-3 custom-shadow-2 border-round surface-card h-full">
+        <div class="flex justify-content-between text-500">
+          <span>Article</span>
+          <span class="inline-flex">
+            <i class="pi pi-clock mr-2"></i>
+            <span>9 min</span>
+          </span>
+        </div>
+        <p class="line-height-3 mt-3 mb-7 text-900 font-medium text-xl">New sustainable energy alternatives and how we can utilize them efficiently.</p>
+        <div>
+          <span class="bg-blue-100 text-blue-600 font-medium py-1 px-3 mr-2" style="border-radius: 30px;">Environment</span>
+          <span class="bg-green-100 text-green-600 font-medium py-1 px-3" style="border-radius: 30px;">Energy</span>
+        </div>
+        <img src="https://blocks.primevue.org/images/blocks/blog/blog-7.png" alt="Image" class="block w-full mt-5">
+      </div>
+    </div>
+    <div class="col-12 lg:col-3 p-3">
+      <div class="p-3 custom-shadow-2 border-round surface-card h-full">
+        <div class="flex justify-content-between text-500">
+          <span>Article</span>
+          <span class="inline-flex">
+            <i class="pi pi-clock mr-2"></i>
+            <span>5 min</span>
+          </span>
+        </div>
+        <p class="line-height-3 mt-3 mb-7 text-900 font-medium text-xl">Upcoming technology is what we've been waiting for to shape our future.</p>
+        <div>
+          <span class="bg-purple-100 text-purple-600 font-medium py-1 px-3 mr-2" style="border-radius: 30px;">Tech</span>
+          <span class="bg-indigo-100 text-indigo-600 font-medium py-1 px-3" style="border-radius: 30px;">Opinion</span>
+        </div>
+        <img src="https://blocks.primevue.org/images/blocks/blog/blog-8.png" alt="Image" class="block w-full mt-5">
+      </div>
+    </div>
+    <div class="col-12 lg:col-3 p-3">
+      <div class="p-3 custom-shadow-2 border-round surface-card h-full">
+        <div class="flex justify-content-between text-500">
+          <span>Article</span>
+          <span class="inline-flex">
+            <i class="pi pi-clock mr-2"></i>
+            <span>5 min</span>
+          </span>
+        </div>
+        <p class="line-height-3 mt-3 mb-7 text-900 font-medium text-xl">Upcoming technology is what we've been waiting for to shape our future.</p>
+        <div>
+          <span class="bg-purple-100 text-purple-600 font-medium py-1 px-3 mr-2" style="border-radius: 30px;">Tech</span>
+          <span class="bg-indigo-100 text-indigo-600 font-medium py-1 px-3" style="border-radius: 30px;">Opinion</span>
+        </div>
+        <img src="https://blocks.primevue.org/images/blocks/blog/blog-8.png" alt="Image" class="block w-full mt-5">
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+    </div>
+</template>
+<style>
+.custom-shadow-2 {
+    background-color: #fff;
+    border-radius: 20px;
+    box-shadow: 0 5px 10px 0 rgba(41,61,102,.2);
+    border: 1px solid #e1e8f5;
+    padding: 24px 20px;
+}
+</style>
