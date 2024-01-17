da1aa380e2867b87f91ca0ea67e82342e82e3b01
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-01-17 14:42:21 +0200

    Updated login post

--- server/routes/auth/login.post.ts
+++ server/routes/auth/login.post.ts
@@ -1,9 +1,10 @@
-import { createJwtToken } from "@/jwt/index";
+import { createAppJwtToken } from "~/vendors/jwt";
 import { prisma } from "~~/prisma/db";
-import bcrypt from "bcrypt";
+import argon2 from "argon2";
 
 export default defineEventHandler(async (event)=>{
-   const {email, password} = await readBody(event);
+   const {data: {email, password}} = await readBody(event);
+   const JWT_APP_TOKEN_SECRET = process.env.NUXT_PUBLIC_JWT_APP_TOKEN_SECRET;
 //test comment
    //Check if the user exists
    if (!email) {
@@ -12,7 +13,7 @@ export default defineEventHandler(async (event)=>{
         success: false
       };
     }
-   const  user = await prisma.user.findFirst({
+   const  user = await prisma.student.findFirst({
       where: {
          
            email: email,
@@ -27,52 +28,37 @@ export default defineEventHandler(async (event)=>{
 
    if(user){
       //Check if the password hash matched
-      const match = await bcrypt.compare(password, user.password);
+      const match = await argon2.verify(user.password, password);
       
       if(match){
          //Successfully login
          //Create a JWT token
-         const token = await createJwtToken();
-
+         const token = await createAppJwtToken(JWT_APP_TOKEN_SECRET);
          setCookie(event, "token", token);
 
          //Store his last Login IP Address and time
          
-         const [userData, employeeData]  = await prisma.$transaction([
-            prisma.user.update({
-               where: {
-                  email: email
-               },
-               data: {
-                  last_logged_in_at: new Date()
-               }
-            }),
-            prisma.user.findUnique({
-               where: {
-                  email: email
-               },
-            })
-          ])
+         const updateUser = await prisma.student.update({
+            where: {
+               email
+            }, 
+            data: {
+               email: email
+            }
+         }); 
 
          // const company_access = userData.company_access.at(0).access_list;
-         console.log({userData})
+
          // @ts-ignore
          // data ? data.map(item => item.name) : [],
 
-         response['user'] = userData
+         response['user'] = updateUser
          response['success'] = true
-         console.log("this is my user data and permissions",{userData})
          //Store encrpted user daa in cookie
-         let filteredUserData = {
-            id: userData.id,
-            first_name: userData.name,
-            last_name: userData.surname,
-            username: userData.username,
-            email: userData.email,
-            profile: userData.profile,
-          };
-         // setCookie(event, "user", JSON.stringify(userData));
-         setCookie(event, "user", JSON.stringify(filteredUserData));
+         delete updateUser.password;
+         
+         //Store user data in cookie
+         setCookie(event, "user", JSON.stringify(updateUser));
          return response
       }else{
          response['message'] = `The user with email ${email} does not exist, is inactive or the password does not match`
