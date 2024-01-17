86f70f7299ccf46d2e08f0c05c7e8cbb08e89a05
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-01-17 14:41:10 +0200

    updated auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,14 +1,13 @@
-import { useAuthStore } from "../stores/auth";
 
 export default defineNuxtRouteMiddleware (async (to, from) => {
-  const authStore = useAuthStore();
-  const isValid: any = await authStore.me();
-  if(isValid.success === false){
-
-    return navigateTo('/login', {
-
-      external: false
-
+  const token = useCookie('token').value || ""
+  
+  const { data, success }: any = await $fetch('/auth/verify-token', {
+    method: "POST",
+    body: JSON.stringify({
+      token
     })
-  }
-});
+  })
+  console.log(success,data?.is_valid)
+  if(!success || data?.is_valid === false) return navigateTo('/login')
+})
