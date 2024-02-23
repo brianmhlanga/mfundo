4dd36bb64e616daa88d81d08b44c9da4157b7c89
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-02-23 16:02:09 +0200

    updated recruitment store

--- stores/recruitment.ts
+++ stores/recruitment.ts
@@ -716,7 +716,7 @@ export const useRecruitmentStore = defineStore('recruitment', {
          
             var config = { 
                 method: 'post',
-                url: '/recruitment/questionaire/create',
+                url: '/recruitment/exams/create',
                 headers: { 
                     'Content-Type': 'application/json'
                 },
@@ -739,6 +739,35 @@ export const useRecruitmentStore = defineStore('recruitment', {
             console.log({result})
             console.log(result.data.success)
             return result;
+         },
+         async createExamination(info:any) {
+            var data = JSON.stringify({
+               "data": info
+            })
+            var config = {
+              method: "POST",
+              url: '/exams/create',
+              headers: { 
+               'Content-Type': 'application/json'
+              },
+              data : data
+            };
+            const result: any = await axios(config).then(function (response) {
+               return {
+                   data: response.data,
+                   success: true
+                }
+           })
+           .catch(function (error) {
+               console.log(error);
+               return {
+                   success: false
+                }
+           });
+           console.log("my created vacancy")
+           console.log({result})
+           console.log(result.data.success)
+           return result;
          },
          async  createQuestionaire(){
             var data = JSON.stringify({
