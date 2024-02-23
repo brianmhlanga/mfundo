cdc2523b450fb44defe4dede20975350a454f0f5
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-02-23 15:55:57 +0200

    Added user session

--- pages/index.vue
+++ pages/index.vue
@@ -5,3 +5,12 @@
         <LandingThird/>
     </NuxtLayout>
 </template>
+<script lang="ts" setup>
+//@ts-ignore
+const { value: { first_name, last_name, profile, id }} = useCookie('user');
+</script>
+<style>
+.surface-section {
+    background-color: #f2f5fb!important;
+}
+</style>
