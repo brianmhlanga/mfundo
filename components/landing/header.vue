2931934b8595e12172fb57452e625c23814782bb
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-02-23 15:54:57 +0200

    removed section

--- components/landing/header.vue
+++ components/landing/header.vue
@@ -1,18 +1,6 @@
 <template>
-    <div class="relative p-8 overflow-hidden bg-no-repeat bg-cover" style="background-image: url(&quot;/images/school.png&quot;);background-size: cover;height: 1000px;">
-        <div class="text-center myy-6 relative">
-            <div class="text-6xl font-bold mb-1 text-white">Ace Your Exams with</div>
-            <div class="text-6xl text-blue-400 font-bold mb-4">Mfundo!</div>
-            <p class="mt-0 mb-3 line-height-3 text-center mx-auto text-blue-100" style="max-width: 500px;">Independent Exam Creation, Answer Selection, Customizable Time Limits.</p>
-            <div class="flex align-items-center justify-content-center flex-wrap">
-            <button style="height: 75px;width: 220px;font-size: 22px;border-radius: 40px;" class="p-button p-component p-3 mt-3" type="button" aria-label="Notify Me" data-pc-name="button" data-pc-section="root" data-pd-ripple="true">
-                <!---->
-                <span class="p-button-label" data-pc-section="label">Explore Exams</span>
-                <!---->
-                <span role="presentation" aria-hidden="true" data-p-ink="true" data-p-ink-active="false" class="p-ink" data-pc-name="ripple" data-pc-section="root"></span>
-            </button>
-            </div>
-        </div>
+    <div class="relative p-8 overflow-hidden bg-no-repeat bg-cover background">
+
     </div>
 </template>
 <style scoped>
@@ -29,5 +17,18 @@ p {
     font-weight: 400;
     line-height: 25px;
     margin: 0px;
+}
+.background {
+    background-image: url("/images/home.png");
+    background-size: cover;
+    width: auto;
+    height: 450px;
+}
+
+/* Change background image for smaller screens */
+@media (max-width: 768px) {
+    .background {
+        background-image: url("/images/home-mobile.png");
+    }
 }
 </style>
