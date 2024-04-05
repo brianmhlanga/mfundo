2ad3c46bf1eb804f9927d20536f2fd4d22039fdc
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-04-05 16:52:31 +0200

    Updated login layout

--- pages/login.vue
+++ pages/login.vue
@@ -1,49 +1,67 @@
 <template>
-    <NuxtLayout name="default">
-        <div class="block-content">
-        <div class="surface-ground px-4 py-8 md:px-6 lg:px-8 flex align-items-center justify-content-center">
-            <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
-            <div class="text-center mb-5">
-                <img src="/images/blocks/logos/hyper.svg" alt="Image" height="50" class="mb-3">
-                <div class="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-                <span class="text-600 font-medium line-height-3">Don't have an account?</span>
-                <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
-            </div>
-            <div>
-                <label for="email1" class="block text-900 font-medium mb-2">Email</label>
-                <input v-model="email" class="p-inputtext p-component w-full mb-3" data-pc-name="inputtext" data-pc-section="root" id="email1" type="text" placeholder="Email address">
-                <label for="password1" class="block text-900 font-medium mb-2">Password</label>
-                <input v-model="password" class="p-inputtext p-component w-full mb-3" data-pc-name="inputtext" data-pc-section="root" id="password1" type="password" placehoder="Password">
-                <div class="flex align-items-center justify-content-between mb-6">
-              
-                <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</a>
+    <NuxtLayout name="logins">
+    <div class="form-body">
+        <div class="website-logo">
+            <a href="index.html">
+                <div class="logo">
+                    <img class="logo-size" src="/images/mfundo.svg" alt="">
                 </div>
-                <button @click="signIn" class="p-button p-component w-full" type="button" aria-label="Sign In" data-pc-name="button" data-pc-section="root" data-pd-ripple="true">
-                <span class="p-button-icon p-button-icon-left pi pi-user" data-pc-section="icon"></span>
-                <span class="p-button-label" data-pc-section="label">Sign In</span>
-                <!---->
-                <span role="presentation" aria-hidden="true" data-p-ink="true" data-p-ink-active="false" class="p-ink" data-pc-name="ripple" data-pc-section="root"></span>
-                </button>
+            </a>
+        </div>
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="startss">
+                    <a href="index.html">
+                            <img class="logo-size" src="/images/mfundo.svg" alt="">
+                    </a>
+                </div>
+                        
+                <div class="info-holder">
+                    <h3>Unlock your full potential with Mfundo.</h3>
+                    <p>The ultimate online quiz platform. Maximize your learning and achieve more with our innovative tools and engaging quizzes. Start your journey to success today!"</p>
+                    <img src="/logins/images/graphic4.svg" alt="">
+                </div>
             </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <div class="page-links">
+                            <a href="#" class="active">Login</a>
+                        </div>
+                        <form @submit.prevent="signIn">
+                            <input class="form-control marginght" v-model="email" type="text" name="username" placeholder="E-mail Address" required>
+                            <input class="form-control" v-model="password" type="password" name="password" placeholder="Password" required>
+                            <div class="form-button">
+                                <Button :loading="loading" id="submit" type="submit" class="ibtn" :disabled="!email || !password">Login</Button>
+                                 <a href="forget8.html">Forget password?</a>
+                            </div>
+                        </form>
+                        <!-- <div class="other-links">
+                            <span>Or login with</span><a href="#">Facebook</a><a href="#">Google</a><a href="#">Linkedin</a>
+                        </div> -->
+                    </div>
+                </div>
             </div>
         </div>
-        <!---->
-        </div>
-    </NuxtLayout>
+    </div>
+</NuxtLayout>
 </template>
 <script lang="ts" setup>
-import { storeToRefs } from "pinia";
 import { useToast } from "primevue/usetoast";
 import { useAuthStore } from "~/stores/auth";
 const authStore = useAuthStore()
 const toast = useToast()
 const email = ref()
+const loading = ref(false)
 const password = ref()
+
 definePageMeta({
         middleware: ["not-auth"]
 });
 
 const signIn = async () => {
+    loading.value = true;
     let data = {
        email: email.value,
        password: password.value
@@ -51,32 +69,41 @@ const signIn = async () => {
     let result = await authStore.login(data).then((data) => {
         console.log(data)
         if (data?.data?.success) {
+            loading.value = false
             toast.add({severity:'info', summary: 'Success', detail: "Succesfully Login", life: 6000});
             navigateTo('/')
         }
         else {
-                toast.add({severity:'warn', summary: 'Registration failed', detail: "Failed to Register", life: 6000});
+                toast.add({severity:'warn', summary: 'Login failed', detail: data?.data?.message, life: 6000});
+                loading.value = false
         }
     })
 }
 </script>
 <style>
-.surface-section.w-full.md\:w-6.p-6.md\:p-8 {
-    height: 100vh;
+img.logo-size {
+    width: auto !important;
+    float: left !important;
+    height: 60px !important;
+    padding-left: 7px !important;
+    margin-bottom: 26px !important;
 }
-.p-input-icon-right > .p-inputtext {
-    padding-right: 2.5rem;
-    width: 100%;
-    height: 50px;
+body {
+    background-color: #ffffff !important;
 }
-.p-input-icon-right > .p-inputtext {
-    padding-right: 2.5rem;
-    width: 515px;
+.form-content .page-links a {
+    color: #000 !important;
 }
-input.p-inputtext.p-component {
-    width: 100% !important;
+.form-content .form-button .ibtn {
+    background-color: #634bff !important;
+    color: #ffffff !important;
+    -webkit-box-shadow: 0 0 0 rgba(0, 0, 0, 0.16);
+    box-shadow: 0 0 0 rgba(0, 0, 0, 0.16);
 }
-.p-input-icon-right > .p-inputtext {
-    padding-right: 218px !important;
+.form-content .form-button a {
+    color: #000 !important;
+}
+input.form-control.marginght {
+    margin-bottom: 10px !important;
 }
 </style>
