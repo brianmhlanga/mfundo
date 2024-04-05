949b89cdc1e47d74bfcc5ce5071fd1ab9af2d26f
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-04-05 16:51:50 +0200

    New login layout

---
+++ layouts/logins.vue
@@ -0,0 +1,67 @@
+<template>
+    <link rel="stylesheet" type="text/css" href="/logins/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/logins/css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="/logins/css/iofrm-style.css">
+    <link rel="stylesheet" type="text/css" href="/logins/css/iofrm-theme8.css">
+    <!-- Import all the site's css files -->
+    <!-- ======== sidebar-nav start =========== -->
+    <!-- ======== sidebar-nav end =========== -->
+
+    <!-- ======== main-wrapper start =========== -->
+  
+        <!-- ========== header start ========== -->
+     
+        <!-- ========== header end ========== -->
+    
+        <!-- ========== section start ========== -->
+        <body>
+          <slot/>
+        </body>
+
+        <!-- ========== footer start =========== -->
+
+</template>
+
+<script>
+
+export default defineNuxtComponent ( { 
+    head(){
+        return {
+            meta: [
+              { charset: 'utf-8' },
+              { name: 'viewport', content: 'width=device-width, initial-scale=0.7' },
+              {
+                hid: 'description',
+                name: 'Mfundo - Online Education Platform',
+                content: 'Silverlands Holdings Zambia'
+              }
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+              ],
+            script: [
+              {
+                src: "/js/jquery-3.5.1.js"
+              },
+              {
+                src: "/logins/js/jquery.min.js"
+              },
+              {
+                src: "/logins/js/popper.min.js"
+              },
+              {
+                src: "/logins/js/bootstrap.min.js"
+              },
+              {
+                src: "/logins/js/main.js"
+              }
+            ] 
+        } 
+    }
+})
+</script>
+<style scoped>
+body {
+    background-color: #ffffff !important;
+}
+</style>
