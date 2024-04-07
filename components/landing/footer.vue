24a3e5596fa3e35e42cd8f94fe9efd477a06ef15
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-04-07 08:41:37 +0200

    Created footer component

---
+++ components/landing/footer.vue
@@ -0,0 +1,313 @@
+<template>
+    <footer class="bg_img mt-80" data-img="/assets/images/footer/footer-bg.jpg">
+            <div class="footer-support">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 col-sm-10">
+                            <div class="footer-support-item">
+                                <div class="icon">
+                                    <i class="flaticon-phone-call"></i>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title">+216 499 49 47</h5>
+                                    <span class="info">Free Support Line</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-10">
+                            <div class="footer-support-item">
+                                <div class="icon">
+                                    <i class="flaticon-headphone"></i>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title">Support Center</h5>
+                                    <span class="info">365 days full support</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-10">
+                            <div class="footer-support-item">
+                                <div class="icon">
+                                    <i class="flaticon-live"></i>
+                                </div>
+                                <div class="content">
+                                    <h5 class="title">Live Support</h5>
+                                    <span class="info">Write Online Now</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-section oh pos-rel">
+                <div class="course-top-shape">
+                    <img src="/assets/images/course/course-top-shape.png" alt="course">
+                </div>
+                <div class="course-bottom-shape">
+                    <img src="/assets/images/course/course-bottom-shape.png" alt="course">
+                </div>
+                <div class="container">
+                    <div class="footer-top">
+                        <div class="footer-area">
+                            <div class="footer-widget widget-link">
+                                <h5 class="title">Talk to Us</h5>
+                                <ul>
+                                    <li>
+                                        <a href="#0">About us</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Registration</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Blog</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Events</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="footer-widget widget-link">
+                                <h5 class="title">Information</h5>
+                                <ul>
+                                    <li>
+                                        <a href="#0">Membership</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Purchase guide</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Privacy policy</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Terms of service</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="footer-widget widget-link">
+                                <h5 class="title">Support</h5>
+                                <ul>
+                                    <li>
+                                        <a href="#0">Documentation</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">FAQs</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Condition</a>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Release Status</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="footer-widget widget-info">
+                                <h5 class="title">Contact Us</h5>
+                                <ul>
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-map-marker-alt"></i>
+                                        </div>
+                                        <div class="content">
+                                            <span>12/A, Hamilton City Way, Newyork, US</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-phone-alt"></i>
+                                        </div>
+                                        <div class="content">
+                                            <a href="Tel:+880551251558">+8987 5675 754 6</a>
+                                            <a href="Tel:+880551251558">+8987 5675 754 6</a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="icon">
+                                            <i class="fas fa-envelope-open-text"></i>
+                                        </div>
+                                        <div class="content">
+                                            <a href="Mailto:info@exampleweb.com">info@exampleweb.com</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-bottom">
+                        <div class="row align-items-center">
+                            <div class="col-md-8">
+                                <h5 class="title">Subscribe Newsletter</h5>
+                                <form class="footer-subscribe-form">
+                                    <input type="text" placeholder="Enter Your Email" name="email">
+                                    <button type="submit">Subscribe Now</button>
+                                </form>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="thumb">
+                                    <NuxtLink to="/">
+                                        <img src="/images/mfundo.svg" alt="logo">
+                                    </NuxtLink>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="copyright-area">
+                        <div class="left">
+                            <p>&copy; Copyright 2023. All Rights Reserved.</p>
+                        </div>
+                        <div class="right">
+                            <ul class="social-icons">
+                                <li>
+                                    <a href="#0"><i class="fab fa-facebook-f"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#0" class="active"><i class="fab fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#0"><i class="fab fa-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#0"><i class="fab fa-linkedin-in"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+</template>
+<style scoped>
+header {
+    padding: 3px 0px !important;
+}
+ul.menu.d-none.d-lg-flex.flex-wrap {
+    margin: auto;
+}
+.header-area .logo img {
+    max-width: 75%;
+    margin-left: 50px;
+}
+a.custom-button {
+    color: #ffffff;
+    text-transform: none !important;
+    line-height: 48px;
+    background: #634bff;
+    padding: 0 35px;
+    display: inline-block;
+    /* text-transform: uppercase; */
+    cursor: pointer;
+    font-weight: 500;
+    font-size: 16px;
+    border-radius: 25px;
+    border: 1px solid #0071f2;
+}
+.banner-overlay::before {
+    left: 0;
+    bottom: 0;
+    top: 0;
+    right: 0;
+    background: #0f1048 !important;
+}
+.banner-section {
+    padding: 113px 0 105px !important;
+}
+h1.titlee {
+    font-family: Poppins, sans-serif;
+    font-size: 60px;
+    font-weight: 500;
+    color: white;
+}
+content.cl-white p {
+    font-family: Poppins, sans-serif !important;
+    margin-top: 10px !important;
+    font-size: 24px !important;
+    line-height: 1.5 !important;
+    color: rgba(255, 255, 255, 0.86);
+}
+p.minidescription {
+    font-family: Poppins, sans-serif !important;
+    margin-top: 10px !important;
+    font-size: 24px !important;
+    line-height: 1.5 !important;
+    color: rgba(255, 255, 255, 0.86);
+}
+.feature-item {
+    padding: 40px 20px;
+    text-align: center;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    border: 1px solid #E2E4FF;
+    -webkit-transition: all ease 0.3s;
+    -moz-transition: all ease 0.3s;
+    transition: all ease 0.3s;
+    margin: 0 auto 30px;
+    -webkit-transition: all ease 0.3s;
+    -moz-transition: all ease 0.3s;
+    transition: all ease 0.3s;
+    vertical-align: top;
+    /* height: 120px; */
+    border-radius: 5px;
+    background-color: #fff;
+    box-shadow: 0 5px 10px 0 rgba(41,61,102,.2);
+    cursor: pointer;
+    color: #2d3040;
+    text-decoration: none;
+    position: relative;
+    overflow: hidden;
+    padding: 41px;
+    z-index: 1;
+    transition: box-shadow .2s;
+    -webkit-transition: box-shadow .2s;
+    /* display: b
+lock; */
+}
+body {
+    padding: 0;
+    margin: 0;
+    font-size: 16px;
+    color: #4B5673;
+    /* color: #7884A7; */
+    line-height: 28px;
+    overflow-x: hidden;
+    font-family: Poppins, sans-serif;
+    background: #f1f5f9 !important;
+}
+.p-button:enabled:hover {
+    background: #ffffff;
+    color: #2eca7f;
+    border-color: #2eca7f;
+}
+a.custom-buttonn {
+    color: #000000 !important;
+    /* text-transform: uppercase; */
+    line-height: 48px;
+    background: #ffffff00 !important; 
+    padding: 0 35px;
+    display: inline-block;
+    text-transform: none!important;
+    font-weight: 500;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 25px;
+    border: 1px solid #3ec3ab;
+    margin-right: 6px;
+}
+a.custom-button.btn-md.theme-one {
+    background-color: #3ec3ab !important;
+    border-color: #0c0c0c !important;
+}
+a.custom-button.btn-md.theme-one:hover {
+    color: white !important;
+}
+.menu li a {
+    padding: 7px !important;
+    font-weight: 500 !important;
+    font-family: Poppins, sans-serif !important;
+    color: #07294D !important;
+    text-transform: capitalize !important;
+    position: relative !important;
+}
+</style>
